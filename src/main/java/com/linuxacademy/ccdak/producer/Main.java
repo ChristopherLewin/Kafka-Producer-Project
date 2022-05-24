93f50e9bfd940b9fe523185a0f1da0718564673e
Chris Lewin <chris.lewin@withyouwithme.com>
2022-05-24 18:05:50 -0500

    init commit

---
+++ src/main/java/com/linuxacademy/ccdak/producer/Main.java
@@ -0,0 +1,43 @@
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Producer {
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        props.put("acks", "all");
+
+        Producer<String, String> producer = new KafkaProducer<>(props);
+
+        try {
+            File file = new File(Main.class.getClassLoader().getResource("sample_transaction_log.txt").getFile());
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] lineArray = line.split(":");
+                String key = lineArray[0];
+                String value = lineArray[1];
+                producer.send(new ProducerRecord<>("inventory_purchases", key, value));
+                if (key.equals("apples")) {
+                    producer.send(new ProducerRecord<>("apple_purchases", key, value));
+                }// end of if
+            }//end of while loop
+            br.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }//end of catch
+
+        producer.close();
+    }// end of Main()
+
+}// end of Producer Class
